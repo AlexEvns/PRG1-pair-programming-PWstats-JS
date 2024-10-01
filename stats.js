e6b2fa4bbbae4e162b0770b6f1b49eb22fc122f0
Alex Evans <alex.evans@ada.ac.uk>
2024-10-01 10:59:36 +0000

    Appending to CSV using Objects

--- stats.js
+++ stats.js
@@ -4,6 +4,9 @@ const inputFile = "10000-most-common-passwords.csv";
 const outputFile = "statistics.csv";
 const delimiter = ",";
 
+const passwordLengths = {};
+
+
 function deleteExistingOutputFile() {
   if (fs.existsSync(outputFile)) {
     fs.unlinkSync(outputFile);
@@ -14,10 +17,21 @@ function processData() {
   const data = fs.readFileSync(inputFile, "utf-8");
   const lines = data.split(/\n/);
 
-  for (line of lines) {
-    elements = line.split(delimiter);
-    console.log(elements);
+  for(const line of lines) {  
+    const element = line.split(delimiter); 
+    const key = element[1].length;
+
+    if(passwordLengths[key]) {
+      passwordLengths[key]++;
+    } else {
+      passwordLengths[key] = 1;
+    }
   }
+  console.log(passwordLengths);
+
+  Object.keys(passwordLengths).forEach(key => {
+    fs.appendFileSync(outputFile, `Chars: ${key}  Count:${passwordLengths[key]}\n`);
+  });
 }
 
 // Main execution
